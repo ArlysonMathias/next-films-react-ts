3c61e68b1d0691693ed3a575cc82bd78abbd26fd
Arlyson Mathias <mathiassouza10@outlook.com>
2022-08-02 14:17:56 -0300

    feature: created component 'ListFilms' and implemented page 'Home'

---
+++ src/types.ts
@@ -0,0 +1,41 @@
+export interface User {
+    id?: string;
+    name: string;
+    email: string;
+    password: string;
+    categoryId: string;
+    createdAt?: Date;
+    updatedAt?: Date;
+}
+
+export interface Film {
+    id?: string;
+    name: string;
+    image: string;
+    description: string;
+    categoryId?: string;
+    createdAt?: Date;
+    updatedAt?: Date;
+}
+
+export interface Categories {
+    id?: string;
+    name: string;
+    createdAt?: Date;
+    updatedAt?: Date;
+}
+
+export interface Favorites {
+    id?: string;
+    filmName: string;
+    userId: string;
+    favoritedAt?: Date;
+}
+
+export interface Perfil {
+    id?: string
+    nickname: string;
+    userId: string;
+    createdAt?: Date;
+    updatedAt?: Date;
+}
